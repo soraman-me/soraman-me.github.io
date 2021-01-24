cb87b7c75b99d10661c81bf983eb6c9d9addc93a
SoraMane <31667047+SoraMane@users.noreply.github.com>
2021-01-24 20:49:56 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -40,11 +40,9 @@ $(document).ready(function () {
         gallery: { enabled: true }
     });
     
-
-        //Get the button:
+    //to-top-button
     mybutton = document.getElementById("myBtn");
 
-    // When the user scrolls down 20px from the top of the document, show the button
     window.onscroll = function() {scrollFunction()};
 
     function scrollFunction() {
@@ -53,11 +51,5 @@ $(document).ready(function () {
     } else {
         mybutton.style.display = "none";
     }
-    }
-
-    // When the user clicks on the button, scroll to the top of the document
-    function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     }
 });
